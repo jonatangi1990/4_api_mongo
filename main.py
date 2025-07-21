d968a9805f550adc15db2b162fb79a21793657cd
Jonatan <jgutierreziglesias@gmail.com>
2025-07-21 11:48:26 +0200

    post crear libro terminado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastApi
+from fastapi import FastAPI
 from routes import book_routes
 
-app = FastApi()
+app = FastAPI()
 
 #Ruta de acceso a los libros
 app.include_router(book_routes.router, prefix="/books", tags=['Books'])
